ad11bb81d49aafb8034e8d5d5dbf6053ce78c6c1
Manaswini <manaswiniyadav8272@gmail.com>
2026-06-27 14:28:38 +0530

    Refactor ResumeMatcher to use direct model import

--- src/matcher.py
+++ src/matcher.py
@@ -1,12 +1,8 @@
 import spacy
-import os
+# Import the model directly as a python package
+import en_core_web_sm
 
 class ResumeMatcher:
     def __init__(self):
-        # Dynamically download en_core_web_sm if it's not present in the environment
-        try:
-            self.nlp = spacy.load("en_core_web_sm")
-        except OSError:
-            print("SpaCy model not found. Downloading dynamically...")
-            os.system("python -m spacy download en_core_web_sm")
-            self.nlp = spacy.load("en_core_web_sm")
+        # Load the directly imported model layout
+        self.nlp = en_core_web_sm.load()
